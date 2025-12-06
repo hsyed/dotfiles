0427007da0411ef28cb285f643e6ce4edd0489a4
Hassan Syed <h.a.syed@gmail.com>
2025-12-06 11:06:06 +0000

    EDITOR=nvim (for claude to pickup)

--- home/home.nix
+++ home/home.nix
@@ -72,7 +72,7 @@
   #  /etc/profiles/per-user/hsyed/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    EDITOR = "nvim";
   };
 
   # Let Home Manager install and manage itself.
