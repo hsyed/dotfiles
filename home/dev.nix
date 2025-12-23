c63bfbf7188fb39a31dffa46a63d6ff5615aa06a
Hassan Syed <h.a.syed@gmail.com>
2025-12-23 11:13:15 +0000

    bumpity bump

--- home/dev.nix
+++ home/dev.nix
@@ -5,6 +5,7 @@
     pkgs.k9s # Kubernetes CLI
     pkgs.claude-code # AI powered code editor
     pkgs.gh # GitHub CLI
+    pkgs.nushell
     pkgs.rustc # Rust compiler
     pkgs.rustup # Rust toolchain manager
     pkgs.go # Go programming language
