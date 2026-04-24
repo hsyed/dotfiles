d32120aaf96c4aacaf13454c042bd20d1d04065c
Hassan Syed <h.a.syed@gmail.com>
2026-04-24 21:00:28 +0100

    use canonical nix-darwin url

--- flake.nix
+++ flake.nix
@@ -13,7 +13,7 @@
     };
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/*";
     nix-darwin = {
-      url = "github:LnL7/nix-darwin";
+      url = "github:nix-darwin/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
